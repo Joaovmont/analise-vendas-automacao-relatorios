a5f23b7c636bb806fb362f77465db6218b00c793
João Vitor Monteiro Moreira <joaovitor.m.moreira68@gmail.com>
2026-09-01 19:12:07 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import os
+import yagmail
 
 lista_cidades = ["BH", "DF", "Manaus", "Rio", "Salvador", "SP"]
 
@@ -6,25 +8,40 @@ faturamentos = {}
 
 for cidade in lista_cidades:
     vendas_df = pd.read_excel(f"Loja {cidade}.xlsx")
-    faturamento_cidade = sum (vendas_df["Vendas"])
-    faturamentos [cidade] = faturamento_cidade
+    faturamento_cidade = sum(vendas_df["Vendas"])
+    faturamentos[cidade] = faturamento_cidade
 
-ranking_df = pd.DataFrame.from_dict(faturamentos, orient="index", columns = ["Vendas"])
-ranking_df = ranking_df.sort_values(by= "Vendas", ascending=False)
+ranking_df = pd.DataFrame.from_dict(
+    faturamentos,
+    orient="index",
+    columns=["Vendas"]
+)
+
+ranking_df = ranking_df.sort_values(
+    by="Vendas",
+    ascending=False
+)
+
 ranking_df = ranking_df.map("R${:,.2f}".format)
 
 mensagem = f"""
-Prezados, 
-Segue em anexo o ranking das lojas
+Prezados,
+
+Segue em anexo o ranking das lojas:
 
 {ranking_df.to_string().replace(" ", ".")}
 
-Att, João.
+Att,
+João.
 """
 
-import yagmail
+usuario = yagmail.SMTP(
+    os.getenv("EMAIL"),
+    os.getenv("EMAIL_SENHA")
+)
 
-usuario = yagmail.SMTP("jvmmcode@gmail.com" , "#[chave do App]")
-usuario.send(to = "jvmmcode@gmail.com", 
-             subject = "Ranking das lojas", 
-              contents = mensagem)
+usuario.send(
+    to="jvmmcode@gmail.com",
+    subject="Ranking das lojas",
+    contents=mensagem
+)
